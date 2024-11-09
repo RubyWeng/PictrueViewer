69bd3e4a418b240b800a97a5dabb0fed36855289
RubyWeng <ruby.weng777@gmail.com>
2024-11-09 16:04:56 +0800

    Initial commit of my WinForms project

---
+++ HWpictrueBox/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace HWpictrueBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ptBox1 = new System.Windows.Forms.PictureBox();
+            this.txbPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnInput = new System.Windows.Forms.Button();
+            this.btnUp = new System.Windows.Forms.Button();
+            this.btnDown = new System.Windows.Forms.Button();
+            this.btnTime = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnTimeEnd = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.ptBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ptBox1
+            // 
+            this.ptBox1.Location = new System.Drawing.Point(136, 38);
+            this.ptBox1.Name = "ptBox1";
+            this.ptBox1.Size = new System.Drawing.Size(400, 300);
+            this.ptBox1.TabIndex = 0;
+            this.ptBox1.TabStop = false;
+            // 
+            // txbPath
+            // 
+            this.txbPath.Location = new System.Drawing.Point(136, 353);
+            this.txbPath.Multiline = true;
+            this.txbPath.Name = "txbPath";
+            this.txbPath.Size = new System.Drawing.Size(400, 22);
+            this.txbPath.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(76, 356);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "圖片路徑";
+            // 
+            // btnInput
+            // 
+            this.btnInput.Location = new System.Drawing.Point(54, 405);
+            this.btnInput.Name = "btnInput";
+            this.btnInput.Size = new System.Drawing.Size(111, 23);
+            this.btnInput.TabIndex = 3;
+            this.btnInput.Text = "選擇路徑(*.png)";
+            this.btnInput.UseVisualStyleBackColor = true;
+            this.btnInput.Click += new System.EventHandler(this.btnInput_Click);
+            // 
+            // btnUp
+            // 
+            this.btnUp.Location = new System.Drawing.Point(245, 405);
+            this.btnUp.Name = "btnUp";
+            this.btnUp.Size = new System.Drawing.Size(75, 23);
+            this.btnUp.TabIndex = 4;
+            this.btnUp.Text = "<<";
+            this.btnUp.UseVisualStyleBackColor = true;
+            this.btnUp.Click += new System.EventHandler(this.btnUp_Click);
+            // 
+            // btnDown
+            // 
+            this.btnDown.Location = new System.Drawing.Point(342, 405);
+            this.btnDown.Name = "btnDown";
+            this.btnDown.Size = new System.Drawing.Size(75, 23);
+            this.btnDown.TabIndex = 5;
+            this.btnDown.Text = ">>";
+            this.btnDown.UseVisualStyleBackColor = true;
+            this.btnDown.Click += new System.EventHandler(this.btnDown_Click);
+            // 
+            // btnTime
+            // 
+            this.btnTime.Location = new System.Drawing.Point(473, 405);
+            this.btnTime.Name = "btnTime";
+            this.btnTime.Size = new System.Drawing.Size(75, 23);
+            this.btnTime.TabIndex = 6;
+            this.btnTime.Text = "幻燈片";
+            this.btnTime.UseVisualStyleBackColor = true;
+            this.btnTime.Click += new System.EventHandler(this.btnTime_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnTimeEnd
+            // 
+            this.btnTimeEnd.Location = new System.Drawing.Point(554, 405);
+            this.btnTimeEnd.Name = "btnTimeEnd";
+            this.btnTimeEnd.Size = new System.Drawing.Size(75, 23);
+            this.btnTimeEnd.TabIndex = 7;
+            this.btnTimeEnd.Text = "結束幻燈片";
+            this.btnTimeEnd.UseVisualStyleBackColor = true;
+            this.btnTimeEnd.Click += new System.EventHandler(this.btnTimeEnd_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(676, 466);
+            this.Controls.Add(this.btnTimeEnd);
+            this.Controls.Add(this.btnTime);
+            this.Controls.Add(this.btnDown);
+            this.Controls.Add(this.btnUp);
+            this.Controls.Add(this.btnInput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txbPath);
+            this.Controls.Add(this.ptBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ptBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ptBox1;
+        private System.Windows.Forms.TextBox txbPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnInput;
+        private System.Windows.Forms.Button btnUp;
+        private System.Windows.Forms.Button btnDown;
+        private System.Windows.Forms.Button btnTime;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnTimeEnd;
+    }
+}
+
---
+++ HWpictrueBox/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace HWpictrueBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private static string filePath = ""; 
+        private static string[] fileArray = new string[] { };
+        private int numbers = 0,location=0;//宣告檔案的數量numbers,當前的圖片位置location
+        private void btnInput_Click(object sender, EventArgs e)
+        {           
+            FolderBrowserDialog openFile = new FolderBrowserDialog();            
+            
+            if (openFile.ShowDialog()==DialogResult.OK)
+            {
+                //input
+                filePath = openFile.SelectedPath;//將輸入的路徑存到fillPath
+                fileArray = Directory.GetFiles(filePath, "*.png", SearchOption.AllDirectories);//將檔名輸入陣列
+                numbers = fileArray.Length;
+
+                //output             
+                location = 0;//當有資料輸入將位置歸零
+                output();
+            }
+          
+            
+        }
+
+        private void btnUp_Click(object sender, EventArgs e)
+        {
+            if (location!=0)
+            {
+                location--;
+                output();
+            }
+            else
+            {
+                location = numbers - 1;
+                output();
+            }
+        }
+
+        private void btnDown_Click(object sender, EventArgs e)
+        {
+            if (location!= numbers-1)
+            {
+                location++;
+                output();
+            }
+            else
+            {
+                location = 0;
+                output();
+            }
+        }
+
+        private void btnTime_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled=true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (location != numbers - 1)
+            {
+                location++;
+                output();
+            }
+            else
+            {
+                location = 0;
+                output();
+            }
+        }
+
+        private void btnTimeEnd_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+
+        void output()
+        {
+            ptBox1.ClientSize = new Size(400, 300);
+            ptBox1.Image = Image.FromFile(fileArray[location]);
+            txbPath.Text = fileArray[location];
+        }
+
+    }
+}
